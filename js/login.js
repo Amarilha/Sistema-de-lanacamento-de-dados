4a0fe3a4bdbfc5c29c286da352837c5cb1e2f843
Igor Amarilha <120053865+Amarilha@users.noreply.github.com>
2025-02-10 17:33:23 -0300

    auth js

---
+++ js/login.js
@@ -0,0 +1,37 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC28JGrAX008bZxSb4-YFkhlmd60U9FKqg",
+    authDomain: "sldd-f3e05.firebaseapp.com",
+    databaseURL: "https://sldd-f3e05-default-rtdb.firebaseio.com",
+    projectId: "sldd-f3e05",
+    storageBucket: "sldd-f3e05.firebasestorage.app",
+    messagingSenderId: "986200949872",
+    appId: "1:986200949872:web:1d8460d3e0981682c7a0b5",
+    measurementId: "G-ZZ7FYTYKD0"
+  };
+
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+console.log(auth);
+
+// Função de login
+window.login = function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+          console.log("Usuário autenticado:", userCredential.user);
+          alert("Login bem-sucedido!");
+      })
+      .catch((error) => {
+          console.error("Erro ao fazer login:", error.message);
+          alert("Erro: " + error.message);
+      });
+};
